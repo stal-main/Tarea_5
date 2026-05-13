f0a9b618b1520974cf66ff3f2eadee422303183c
Kevin Solano <7stalyakov7@gmail.com>
2026-05-13 00:56:56 -0600

    Update Tarea_5.cpp

--- Tarea_5.cpp
+++ Tarea_5.cpp
@@ -1,9 +1,44 @@
 
 #include <iostream>
+#include <stdexcept>
 #include "Matrix.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using std::cout;
+using std::cin;
+using std::endl;
+
+void printMenu() {
+
+
+}
+
+int main() {
+
+    int rows;
+
+    cout << "";
+
+    cin >> rows;
+
+    int cols;
+
+    cout << "";
+
+    cin >> cols;
+
+    Matrix<int>* m = nullptr;
+
+    try {
+
+        m = new Matrix<int>(rows, cols);
+    }
+
+    catch (runtime_error& e) {
+
+    }
+
+    delete m;
+
+    return 0;
 }
 
